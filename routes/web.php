00a48a9221080d2fc32ac4b72b4e93612d300286
lekiagospel@gmail.com <lekiagospel@gmail.com>
2019-12-10 23:17:33 +0100

    re-initialize the project

--- routes/web.php
+++ routes/web.php
@@ -10,18 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
- Route::get('/login', function () {
-    return view('login');
- });
-Route::get('/', 'PostsController@index');//my home page does not show posts;
-Route::get('posts/create', 'PostsController@create')->name('posts.create');
-Route::get('posts/{post}', 'PostsController@show')->name('posts.show');
-Route::post('/posts', 'PostsController@store')->name('posts.store');
-Route::post('posts/{post}/comments', 'CommentsController@store')->name('posts.comment');
-Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/register', 'RegistrationController@create');
-Route::post('/register', 'RegistrationController@store');
-Route::get('/login', 'SessionsController@create');
-Route::post('/logout', 'Sessions@Contoller@destroy');
+Route::get('/', function () {
+    return view('welcome');
+});
